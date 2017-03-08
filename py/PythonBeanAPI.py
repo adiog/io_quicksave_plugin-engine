742956add8fe00620fef1f3968fecb2e30048fcc
Aleksander Gajewski <adiog@brainfuck.pl>
2017-03-08 16:55:58 +0100

    sample

--- py/PythonBeanAPI.py
+++ py/PythonBeanAPI.py
@@ -2,6 +2,9 @@ from generated.QsBeans import ItemBean, TagBean, RichItemBean
 from pybeans import to_string
 
 def process(itemBean):
-    return RichItemBean(item=itemBean, tags=[TagBean(value='#plugin-engine')])
+    tags = [TagBean(name='chrome')]
+    if ('wikipedia' in itemBean.source_url):
+        tags.append(TagBean(name='wiki'))
+    return RichItemBean(item=itemBean, tags=tags)
 
 
