47f1bfc533ac64c70c8a09e0f3116157624619d1
Aleksander Gajewski <adiog@brainfuck.pl>
2017-08-25 19:44:41 +0200

    up

--- js/metatypes/code.js
+++ js/metatypes/code.js
@@ -3,4 +3,11 @@
  */
 
 const CodeMetaType =
-    {};
+    {
+        content:
+            function (ecmaItem) {
+                let contentDom = null;
+                contentDom = $$$(pre({class: 'prettyprint', style: 'text-align: left'}), ecmaItem.metaBean.text);
+                return contentDom;
+            }
+    };
